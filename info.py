a2bd8da11a1f2d76cbfcd357d1bf39953d42d520
matizxe <129849674+matizxe@users.noreply.github.com>
2023-10-05 17:03:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tamilmov192:tamilmov192@cluster0.btsivx2.mongodb.net/?retryWrites=true&w=majority
-")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tamilmov192:tamilmov192@cluster0.btsivx2.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "new")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
