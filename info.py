3d296b1ab63a134cb6bc83a7ae916998932e8c36
matizxe <129849674+matizxe@users.noreply.github.com>
2024-02-18 21:21:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tamilmov192:tamilmov192@cluster0.btsivx2.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "new")
+DATABASE_NAME = environ.get('DATABASE_NAME', "news")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
