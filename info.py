4a5722ae9b2132143d5e75890b9011cd3d9e8d44
xramze <166788107+xramze@users.noreply.github.com>
2024-04-13 09:31:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '19553188'))
 API_HASH = environ.get('API_HASH', '52c7a1c621bc52258f55961d65028bb0')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6440038398:AAFCUGXMDoaVFW0_2CYLaKNKI0e9oZtF9_A")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7161247250:AAH7QSHCzDqFiHc0f46KCDC9UL1dO-N8y4o")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -28,7 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1354643852').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001645540092').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001757084035').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -36,8 +36,8 @@ auth_channel = environ.get('AUTH_CHANNEL','-1001757084035')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001983067472')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001756733827')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1004113613157')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002034255557')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
@@ -56,13 +56,13 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/TamilMov_chat')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Yv5RDWsaOrw2YzE1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TamilMov_LinkZz')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/TamilMov_Tutorial/3')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001609124536'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+0RPSI6Vj7_IwNjFl')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002082477753'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+Yv5RDWsaOrw2YzE1')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
