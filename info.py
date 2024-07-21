ca4cdaebbf08258fb6f45d85628b7403d5ac977a
xramze <166788107+xramze@users.noreply.github.com>
2024-07-21 21:29:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Tamilmovprebot:6BUjMX6ZuLhQFmxo@cluster0.7p2b7ef.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tamilmovbot1:g8V6OQIU5IpqGJTq@cluster0.niuo3aq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Tamilmovprebot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
